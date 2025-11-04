6b6598f54c3415d626734566c99e2b02f828a8d9
Kyrylo Kuzyk <kuzykkyrylo@meta.com>
2025-11-04 16:31:40 +0100

    - Use PassthroughCameraAccess instead of WebCamTextureManager. Migration guide: https://developers.meta.com/horizon/documentation/unity/unity-pca-migration-from-webcamtexture - Update to MRUK v81. Release notes: https://developers.meta.com/horizon/downloads/package/meta-xr-mr-utility-kit-upm/81.0 - Update to Unity 6000.0.61f1 - Use OpenXR provider instead of Oculus

---
+++ Assets/PassthroughCameraApiSamples/PassthroughCamera/Scripts/RequestPermissionsOnce.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PassthroughCameraSamples
+{
+    internal static class RequestPermissionsOnce
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void AfterSceneLoad()
+        {
+            bool permissionsRequestedOnce = false;
+            SceneManager.sceneLoaded += (scene, _) =>
+            {
+                if (scene.name != "StartScene")
+                {
+                    if (!permissionsRequestedOnce)
+                    {
+                        permissionsRequestedOnce = true;
+                        OVRPermissionsRequester.Request(new[]
+                        {
+                            OVRPermissionsRequester.Permission.Scene,
+                            OVRPermissionsRequester.Permission.PassthroughCameraAccess
+                        });
+                    }
+                }
+            };
+        }
+    }
+}
